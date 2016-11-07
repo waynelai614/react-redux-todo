78eeeb70db0773ccd85c6922b41531154e50da6c
Wayne Lai <waynelai614@gmail.com>
2016-11-07 22:08:38 +0800

    edit Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Hero = () => (
   <section className="hero is-primary is-bold">
     <div className="hero-body">
-      <div className="container">
-        <h1 className="title">
+      <div className="container has-text-centered">
+        <h1 className="title" style={{ fontSize: '4.5em' }}>
           Todo
         </h1>
         <h2 className="subtitle">
-          react.js, redux
+          Double-click to edit a todo
         </h2>
       </div>
     </div>
