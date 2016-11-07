064515ab7250579aa8c6d4954cc53c61e1f4c5ba
Wayne Lai <waynelai614@gmail.com>
2016-11-07 10:39:39 +0800

    store if todos has change

--- src/redux/middleware/saveToLocalStorage.js
+++ src/redux/middleware/saveToLocalStorage.js
@@ -1,8 +1,12 @@
 import todoUtils from '../utils'
 
 const saveToLocalStorage = store => next => action => {
+  let preTodos = store.getState().todos
   let result = next(action)
-  todoUtils.setTodosToClient(store.getState().todos)
+  let nextTodos = store.getState().todos
+  if (preTodos !== nextTodos) {
+    todoUtils.setTodosToClient(store.getState().todos)
+  }
   return result
 }
 
